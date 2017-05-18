a3142cca1fe8df49dde9b067b8395e6d6fa05ec8
mmorii <morii@ism.ac.jp>
2017-05-18 12:16:05 +0900

    modify

--- setup/setup.sh
+++ setup/setup.sh
@@ -29,9 +29,12 @@ HEADAS_VER=6.16
 export HEADAS=/soft/heasoft/heasoft-${HEADAS_VER}/x86_64-unknown-linux-gnu-libc2.12
 source $HEADAS/headas-init.sh
 
+
 ################################################################
 ################################################################
 ############ Don't Edit Below. #################################
+
+export PGPLOT_TYPE=/xw
 
 ##
 ## change the terminal title
