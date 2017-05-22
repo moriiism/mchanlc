dfeb4ed3da4d446aa6bf940bf9c32ad5653d40ab
mmorii <morii@ism.ac.jp>
2017-05-22 16:29:07 +0900

    modify

--- script/fista.mat2.weight.S.R
+++ script/fista.mat2.weight.S.R
@@ -22,8 +22,7 @@ args <- commandArgs(TRUE)
 infile1     = args[1]
 infile2     = args[2]
 freq.file   = args[3]
-mat.file    = args[4]
-mat.w.file  = args[5]
-nlambda     = as.integer(args[6])
+lambda      = as.numeric(args[4])
+outdir      = args[5]
 
-fista.mat2.weight(infile1, infile2, freq.file, mat.file, mat.w.file, nlambda)
+fista.mat2.weight(infile1, infile2, freq.file, lambda, outdir)
--- scriptR/fista.mat2.weight.R
+++ scriptR/fista.mat2.weight.R
@@ -7,164 +7,27 @@
 ###
 
 mitooldir = "/home/morii/work/github/moriiism/mitool"
+mchanlcdir = "/home/morii/work/github/moriiism/mchanlc"
 
 ###
 
 source( paste(mitooldir, "script/mirlib/iolib.R", sep="/") )
+source( paste(mitooldir, "script/mirlib/binning.R", sep="/") )
+source( paste(mchanlcdir, "scriptR/fista.lib.R", sep="/") )
 options(digits=10)
-library(grplasso)
 library(plotrix)
 
-###
-### a sin(theta) + b cos(theta) = sqrt(a**2 + b**2) * sin(theta + alpha)
-###
-GetAlphaOfSinCos <- function(a, b)
+fista.mat2.weight <- function(infile1, infile2, freq.file, lambda, outdir)
 {
-    dist = sqrt(a**2 + b**2)
-    cos.alpha = a / dist
-    sin.alpha = b / dist
-    if(sin.alpha >= 0.0){
-        alpha = acos(cos.alpha)
-    }
-    else {
-        alpha = - acos(cos.alpha)
-    }
-    return(alpha)
-}
-
-###
-### 0 <= delta.time <= period
-###
-GetDeltaTime <- function(time, period)
-{
-    delta.time = time %% period
-}
-
-
-
-
-GetIbin <- function(xval, xlo, xup, nbinx)
-{
-    delta.xval = (xup - xlo) / nbinx
-    ibin = ceiling((xval - xlo) / delta.xval)
-    return(ibin)
-}
-
-###SoftThres <- function(x, lambda)
-###{
-###    if(lambda < x){
-###        ans = x - lambda
-###    } else if(-lambda <= x && x <= lambda){
-###        ans = 0.0
-###    } else {
-###        ans = x + lambda
-###    }
-###    return(ans)
-###}
-###
-###SoftThres2 <- function(a, b, L, lambda)
-###{
-###    len = sqrt(a**2 + b**2)
-###    ans = 1./L / len * SoftThres(L * len, lambda)
-###    return(ans)
-###}
-
-SoftThres2 <- function(a, b, c, d, L, lambda)
-{
-    l = sqrt(a**2 + b**2 + c**2 + d**2) * L
-    ans = 0.0
-    if(lambda < l){
-        ans = 1.0 - lambda / l
-    }
-    if(-lambda <= l && l <= lambda){
-        ans = 0.0
-    }
-    if(l < -lambda){
-        ans = 1.0 + lambda / l
-    }
-    return(ans)
-}
-
-
-ProxMap <- function(y, L, A.mat, h.vec, lambda)
-{
-    b = y - 2.0/L * t(A.mat) %*% (A.mat %*% y - h.vec)
-    N2 = length(b)
-    N = N2 / 2
-    b.new = rep(0.0, 2 * N)
-    for(i in 1:(N/2)){
-        factor = SoftThres2(b[i], b[i + N/2], b[i + N], b[i + N/2 * 3], L, lambda)
-        b.new[i]       = b[i] * factor
-        b.new[i + N/2] = b[i + N/2] * factor
-        b.new[i + N]   = b[i + N] * factor
-        b.new[i + N/2 * 3] = b[i + N/2 * 3] * factor
-    }
-    return(b.new)
-}
-
-GFunc <- function(J.vec, lambda){
-    N2 = length(J.vec)
-    N = N2 / 2
-    sum = 0.0
-    for(i in 1:(N/2)){
-        sum = sum + sqrt(J.vec[i]**2 + J.vec[i + N/2]**2 + J.vec[i + N]**2 + J.vec[i + N/2 * 3]**2)
-    }
-    ans = lambda * sum
-    return(ans)
-}
-
-DiffFFunc <- function(y, A.mat, h.vec){
-    ans = 2 * t(A.mat) %*% (A.mat %*% y - h.vec)
-    return(ans)
-}
-
-FFunc <- function(J.vec, A.mat, h.vec){
-    vec.tmp = h.vec - A.mat %*% J.vec
-    ans = sum( vec.tmp * vec.tmp )
-    return(ans)
-}
-
-QFunc <- function(x, y, L, A.mat, h.vec, lambda){
-    term1 = FFunc(y, A.mat, h.vec)
-    term2 = sum((x - y) * DiffFFunc(y, A.mat, h.vec))
-    term3 = L / 2. * sum((x - y) * (x - y))
-    term4 = GFunc(x, lambda)
-    ans = term1 + term2 + term3 + term4
-    return(ans)
-}
-
-FGFunc <- function(J.vec, A.mat, h.vec, lambda){
-    ans = FFunc(J.vec, A.mat, h.vec) + GFunc(J.vec, lambda)
-    return(ans)
-}
-
-FindIk <- function(y, L.pre, eta, A.mat, h.vec, lambda){
-    ik.max = 1000
-    ik = 0
-    while(ik <= ik.max){
-        L = eta**ik * L.pre
-        pLy = ProxMap(y, L, A.mat, h.vec, lambda)
-        FGFunc.val = FGFunc(pLy, A.mat, h.vec, lambda)
-        QFunc.val  = QFunc(pLy, y, L, A.mat, h.vec, lambda)
-        ### printf("FGFunc.val = %e, QFunc.val = %e\n", FGFunc.val, QFunc.val)
-        if(FGFunc.val <= QFunc.val){
-            break
-        }
-        ik = ik + 1
-    }
-    return(ik)
-}
-
-fista.mat2.weight <- function(infile1, infile2, freq.file, mat.file, mat.w.file, nlambda)
-{
-    printf(" --- fista.mat ---- \n")
+    printf(" --- fista.mat2.weight ---- \n")
     printf(" infile1    = %s\n", infile1)
     printf(" infile2    = %s\n", infile2)
-    printf(" freq.file = %s\n", freq.file)
-    printf(" mat.file  = %s\n", mat.file)
-    printf(" mat.w.file  = %s\n", mat.w.file)
-    printf(" nlambda   = %d\n", nlambda)
-    printf(" --- fista.mat ---- \n")
+    printf(" freq.file  = %s\n", freq.file)
+    printf(" lambda     = %e\n", lambda)
+    printf(" outdir     = %s\n", outdir)
+    printf(" --- fista.mat2.weight ---- \n")
+
+    dir.create(outdir)
 
     ##
     ## read infile1
@@ -182,7 +45,28 @@ fista.mat2.weight <- function(infile1, infile2, freq.file, mat.file, mat.w.file,
     printf("nrow(data2.df) = %d\n", nrow(data2.df))
     nrow2 = nrow(data2.df)
 
-    data.df = rbind(data1.df, data2.df)
+    ##
+    ## make fourier matrix file
+    ##
+    cmd.bin = "/home/morii/work/github/moriiism/mchanlc/mkmat/mkmat2"
+    cmd = sprintf("%s  %s  %s  %s  %s  %s",
+        cmd.bin, infile1, infile2, freq.file, outdir, "mat")
+    system(cmd)
+
+    ##
+    ## read fourier matrix file
+    ##
+    mat.w.file = sprintf("%s/mat_12_w.dat", outdir)
+    mat.w.df <- read.table(mat.w.file, comment.char = "#")
+    A.w.mat = data.matrix(mat.w.df)
+    printf("nrow(A.w.mat) = %d\n", nrow(A.w.mat))
+    printf("ncol(A.w.mat) = %d\n", ncol(A.w.mat))
+
+    mat.file = sprintf("%s/mat_12.dat", outdir)
+    mat.df <- read.table(mat.file, comment.char = "#")
+    A.mat = data.matrix(mat.df)
+
+#######
 
     ##
     ## read frequency setup file
@@ -191,54 +75,16 @@ fista.mat2.weight <- function(infile1, infile2, freq.file, mat.file, mat.w.file,
     freq.lo = freq.df[1, 1]
     freq.up = freq.df[1, 2]
     nfreq   = freq.df[1, 3]
-
     delta.freq = (freq.up - freq.lo) / nfreq
-    ncol = nfreq * 2 * 2
+    ### ncol = nfreq * 2 * 2
+    
+    nrow1 = nrow(data1.df)
+    nrow2 = nrow(data2.df)
+    ncol  = ncol(A.mat)
 
-    ##
-    ## read fourier matrix file
-    ##
-    mat.w.df <- read.table(mat.w.file, comment.char = "#")
-    A.w.mat = data.matrix(mat.w.df)
-    print("matrix filled")
-
-    printf("nrow(A.w.mat) = %d\n", nrow(A.w.mat))
-    printf("ncol(A.w.mat) = %d\n", ncol(A.w.mat))
-
-
-    mat.df <- read.table(mat.file, comment.char = "#")
-    A.mat = data.matrix(mat.df)
-    print("matrix filled")
-
-    B.mat = cbind(1, A.w.mat)
+    data.df = rbind(data1.df, data2.df)
+    
     h.vec = as.vector(c(data1.df[,2] * sqrt(nrow2), data2.df[,2] * sqrt(nrow1) ))
-
-    ##
-    ## normalize and standardize
-    ##
-    ##mean = mean(h.vec)
-    ##sd   = sd(h.vec)
-    ##h.vec = h.vec - mean
-    ##h.vec = h.vec / sd
-    
-    ##
-    ## calc lambda.array
-    ##
-    index.vec = c(NA, 1:(ncol/4), 1:(ncol/4), 1:(ncol/4), 1:(ncol/4))
-
-    #### adhoc
-    
-##    lambda.array <- lambdamax(B.mat, y = h.vec, index = index.vec,
-##                              penscale = sqrt, model = LinReg()) * 10 * 0.5^(0:(nlambda-2))
-    lambda.array <- lambdamax(B.mat, y = h.vec, index = index.vec,
-                              penscale = sqrt, model = LinReg()) * 200 * 0.8^(0:(nlambda-1))
-    
-##    lambda.array = c(lambda.array, 0.0)
-
-    #### ad hoc
-##    lambda.array = 1e4 * 0.5^(0:(nlambda-2))
-##    lambda.array = c(lambda.array, 0.0)
-    ####
 
     hist.lo = -0.005
     hist.up =  0.005
@@ -246,206 +92,142 @@ fista.mat2.weight <- function(infile1, infile2, freq.file, mat.file, mat.w.file,
     hist.delta = (hist.up - hist.lo) / hist.nbin
     
     tolerance = 5.e-8
-    n.nonzero = numeric(nlambda)
-    for(ilambda in 1:nlambda){
-        lambda = lambda.array[ilambda]
-        printf("lambda = %e\n", lambda)
+    eta = 1.2
+    x = rep(0.0, ncol)
+    x.pre = x
+    y = x
+    L = 1e-3
+    L.pre = L
+    k.max = 500
+    t = 1
 
-        eta = 1.2
-        x = rep(0.0, ncol)
+    cost = 0.0
+    cost.pre = cost
+    for(k in 1 : k.max){
+        ## printf("k = %d\n", k)
+        ik = FindIk(y, L.pre, eta, A.w.mat, h.vec, lambda)
+        ## printf("ik = %d\n", ik)
+        L = eta**ik * L.pre
+        x = ProxMap(y, L, A.w.mat, h.vec, lambda)
+        t.new = (1. + sqrt(1. + 4 * t**2))/2.
+        y.new = x + (t - 1.)/ t.new * (x - x.pre)
         x.pre = x
-        y = x
-        L = 1e-3
+        y = y.new
+        t = t.new
         L.pre = L
-        k.max = 500
-        t = 1
 
-        cost = 0.0
+        cost = FGFunc(y.new, A.w.mat, h.vec, lambda)
+        ##printf("ik = %d, L = %e, t = %e, FGFunc = %e\n",
+        ##       ik, L, t, FGFunc(y.new, A.w.mat, h.vec, lambda))
+
+        ##printf("diff = %e\n", (cost.pre - cost) / cost)
+        
+        if(k > 1 && (cost.pre - cost) / cost < tolerance){
+            printf("k = %d, cost = %e\n", k, cost)
+            break
+        }
         cost.pre = cost
-        for(k in 1 : k.max){
-            ## printf("k = %d\n", k)
-            ik = FindIk(y, L.pre, eta, A.w.mat, h.vec, lambda)
-            ## printf("ik = %d\n", ik)
-            L = eta**ik * L.pre
-            x = ProxMap(y, L, A.w.mat, h.vec, lambda)
-            t.new = (1. + sqrt(1. + 4 * t**2))/2.
-            y.new = x + (t - 1.)/ t.new * (x - x.pre)
-            x.pre = x
-            y = y.new
-            t = t.new
-            L.pre = L
-
-            cost = FGFunc(y.new, A.w.mat, h.vec, lambda)
-            ##printf("ik = %d, L = %e, t = %e, FGFunc = %e\n",
-            ##       ik, L, t, FGFunc(y.new, A.w.mat, h.vec, lambda))
-
-            ##printf("diff = %e\n", (cost.pre - cost) / cost)
-            
-            if(k > 1 && (cost.pre - cost) / cost < tolerance){
-                printf("k = %d, cost = %e\n", k, cost)
-                break
-            }
-            cost.pre = cost
-        }
-
-        n.nonzero[ilambda] = 0
-        for(i in 1:length(x)){
-            if(abs(x[i]) > lambda){
-                n.nonzero[ilambda] = n.nonzero[ilambda] + 1
-            }
-        }
-        printf("n.nonzero = %d\n", n.nonzero[ilambda])
-
-        ##
-        ## delta.time
-        ##
-        N = ncol/4
-        delta.time12.vec = numeric(0)
-        weight.delta.time12.vec = numeric(0)
-
-        delta.time12.vec.tmp = numeric(0)
-        weight.delta.time12.vec.tmp = numeric(0)
-        period.vec.tmp = numeric(0)
-        
-        delta.time1.vec = numeric(0)
-        delta.time2.vec = numeric(0)
-        isel.vec = numeric(0)
-
-        x.common = rep(0.0, ncol)
-        
-        for(i in 1:N){
-            if(abs(x[i]) > 0.0  && abs(x[i + N]) > 0.0 && abs(x[i + 2 * N]) > 0.0 && abs(x[i + 3 * N]) > 0.0 ) {
-                freq = freq.lo + delta.freq * (i - 0.5)
-                omega = 2 * pi * freq
-                delta.time1 = GetAlphaOfSinCos(x[i + N], x[i]) / omega
-                delta.time2 = GetAlphaOfSinCos(x[i + 3 * N], x[i + 2 * N]) / omega
-                delta.time1 = GetDeltaTime(delta.time1, 1./freq)
-                delta.time2 = GetDeltaTime(delta.time2, 1./freq)
-                delta.time12 = delta.time1 - delta.time2
-                weight.delta.time12 = sqrt( x[i]**2 + x[i + N]**2 + x[i + 2 * N]**2 + x[i + 3 * N]**2 )
-
-                ## select common between X and Opt
-                if(0.0 < delta.time12 && delta.time12 < 0.01){
-                    x.common[i] = x[i]
-                    x.common[i + N] = x[i + N]
-                    x.common[i + 2 * N] = x[i + 2 * N]
-                    x.common[i + 3 * N] = x[i + 3 * N]
-                }
-                
-                ##printf("delta.time1 = %e\n", delta.time1)
-                ##printf("delta.time2 = %e\n", delta.time2)                
-                ##printf("delta.time12 = %e\n", delta.time12)
-                ##printf("T = %e\n", 1./freq)
-                ##printf("weight.delta.time12 = %e\n", weight.delta.time12)
-
-                delta.time12.vec.tmp = c(delta.time12.vec.tmp, delta.time1 - delta.time2)
-                weight.delta.time12.vec.tmp = c(weight.delta.time12.vec.tmp, weight.delta.time12)
-                period.vec.tmp = c(period.vec.tmp, 1./freq)
-                
-                if(hist.lo < delta.time12 && delta.time12 < hist.up){
-                    delta.time12.vec = c(delta.time12.vec, delta.time1 - delta.time2)
-                    weight.delta.time12.vec = c(weight.delta.time12.vec, weight.delta.time12)
-                    ##weight.delta.time12.vec = c(weight.delta.time12.vec, 1.0)
-                    
-                    ## ibin = GetIbin(delta.time1 - delta.time2, hist.lo, hist.up, hist.nbin)
-                    ## hist.oval[ibin] = hist.oval[ibin] + 1.0
-                    
-                }
-                isel.vec = c(isel.vec, i)
-            }
-        }
-        hist(delta.time12.vec, breaks=seq(hist.lo,hist.up,hist.delta))
-        ## weighted.hist(delta.time12.vec, weight.delta.time12.vec, breaks=seq(hist.lo,hist.up,hist.delta))
-
-
-#        ## adhoc
-#        for(isel in 1:length(isel.vec)){
-#            xsel = rep(0.0, length(x))
-#            N = length(x)/4
-#            index = isel.vec[isel]
-#            xsel[index] = x[index]
-#            xsel[index + N] = x[index + N]
-#            xsel[index + 2 * N] = x[index + 2 * N]
-#            xsel[index + 3 * N] = x[index + 3 * N]
-#            h.rec.sel.vec = A.w.mat %*% xsel
-#            lc.sel.rec = cbind(data.df[, 1], h.rec.sel.vec)
-#            outfile = sprintf("rec_%2.2d_sel_%2.2d.dat", ilambda, isel)
-#            write(t(lc.sel.rec), file=outfile, ncolumns = 2)
-#        }
-
-        ##
-        ## lag v.s. norm
-        ##
-        lag.norm = cbind(delta.time12.vec.tmp, weight.delta.time12.vec.tmp)
-        outfile = sprintf("lag_norm_%2.2d.dat", ilambda)
-        write(t(lag.norm), file=outfile, ncolumns = 2)
-
-        ##
-        ## lag v.s. norm v.s. period
-        ##
-        lag.norm = cbind(delta.time12.vec.tmp, weight.delta.time12.vec.tmp, period.vec.tmp)
-        outfile = sprintf("lag_norm_period_%2.2d.dat", ilambda)
-        write(t(lag.norm), file=outfile, ncolumns = 3)
-
-        
-       
-        h.rec.vec = A.mat %*% x
-
-        ##
-        ## h.rec.vec = h.rec.vec * sd + mean
-        ##
-        
-        lc.rec = cbind(data.df[, 1], h.rec.vec)
-        outfile = sprintf("rec_%2.2d.dat", ilambda)
-        write(t(lc.rec), file=outfile, ncolumns = 2)
-
-        outfile = sprintf("x_%2.2d.dat", ilambda)
-        x.mat = cbind(c(1:(length(x)/2),1:(length(x)/2)), x)
-        write(t(x.mat), file=outfile, ncolumns = 2)
-
-        ##
-        ## common
-        ##
-        h.rec.common.vec = A.mat %*% x.common
-
-        lc.common.rec = cbind(data.df[, 1], h.rec.common.vec)
-        outfile = sprintf("rec_common.%2.2d.dat", ilambda)
-        write(t(lc.common.rec), file=outfile, ncolumns = 2)
-
-        outfile = sprintf("x_common.%2.2d.dat", ilambda)
-        x.common.mat = cbind(c(1:(length(x.common)/2),1:(length(x.common)/2)), x.common)
-        write(t(x.common.mat), file=outfile, ncolumns = 2)
-
-        power.common = 0.0
-        for(i in 1:N){
-            power.common = power.common + x.common[i]**2 + x.common[i + N]**2 + x.common[i + 2 * N]**2 + x.common[i + 3 * N]**2
-        }
-        printf("power.common = %e\n", power.common)
-
-        power = 0.0
-        for(i in 1:N){
-            power = power + x[i]**2 + x[i + N]**2 + x[i + 2 * N]**2 + x[i + 3 * N]**2
-        }
-        printf("power = %e\n", power)
-
-        
-        ##
-        ## uncommon 
-        ##
-                
-        lc.uncommon.rec = cbind(data.df[, 1], h.rec.vec - h.rec.common.vec)
-        outfile = sprintf("rec_uncommon.%2.2d.dat", ilambda)
-        write(t(lc.uncommon.rec), file=outfile, ncolumns = 2)
-        
     }
 
-    for(ilambda in 1:nlambda){
-        lambda = lambda.array[ilambda]
-        printf("%d: lambda = %e, n.nonzero[ilambda] = %d \n",
-               ilambda, lambda, n.nonzero[ilambda])
-    }
+    ##
+    ## delta.time
+    ##
+    N = ncol/4
+    delta.time12.vec = numeric(0)
+    norm.delta.time12.vec = numeric(0)
+    norm.opt.delta.time12.vec = numeric(0)
+    norm.xray.delta.time12.vec = numeric(0)
+    period.vec = numeric(0)
+
+    x.lagplus  = rep(0.0, ncol)
+    x.lagminus = rep(0.0, ncol)
     
+    for(i in 1:N){
+        if(abs(x[i]) > 0.0  && abs(x[i + N]) > 0.0 && abs(x[i + 2 * N]) > 0.0 && abs(x[i + 3 * N]) > 0.0 ) {
+            freq = freq.lo + delta.freq * (i - 0.5)
+            omega = 2 * pi * freq
+            delta.time1 = GetAlphaOfSinCos(x[i + N], x[i]) / omega
+            delta.time2 = GetAlphaOfSinCos(x[i + 3 * N], x[i + 2 * N]) / omega
+            delta.time1 = GetDeltaTime(delta.time1, 1./freq)
+            delta.time2 = GetDeltaTime(delta.time2, 1./freq)
+            delta.time12 = delta.time1 - delta.time2
+            
+            norm.delta.time12      = sqrt( x[i]**2 + x[i + N]**2 + x[i + 2 * N]**2 + x[i + 3 * N]**2 )
+            norm.opt.delta.time12  = sqrt( x[i]**2 + x[i + N]**2)
+            norm.xray.delta.time12 = sqrt( x[i + 2 * N]**2 + x[i + 3 * N]**2)
+
+            ## select lagplus between X and Opt
+            if(0.0 <= delta.time12 ){
+                x.lagplus[i] = x[i]
+                x.lagplus[i + N] = x[i + N]
+                x.lagplus[i + 2 * N] = x[i + 2 * N]
+                x.lagplus[i + 3 * N] = x[i + 3 * N]
+            }
+            if(0.0 > delta.time12 ){
+                x.lagminus[i] = x[i]
+                x.lagminus[i + N] = x[i + N]
+                x.lagminus[i + 2 * N] = x[i + 2 * N]
+                x.lagminus[i + 3 * N] = x[i + 3 * N]
+            }
+
+
+            
+            delta.time12.vec = c(delta.time12.vec, delta.time12)
+            norm.delta.time12.vec = c(norm.delta.time12.vec, norm.delta.time12)
+            norm.opt.delta.time12.vec = c(norm.opt.delta.time12.vec, norm.opt.delta.time12)
+            norm.xray.delta.time12.vec = c(norm.xray.delta.time12.vec, norm.xray.delta.time12)
+            period.vec = c(period.vec, 1./freq)
+        }
+    }
+
+    ##
+    ## lag, norm, norm.opt, norm.xray, period
+    ##
+    sum.df = cbind(delta.time12.vec, norm.delta.time12.vec, norm.opt.delta.time12.vec, norm.xray.delta.time12.vec, period.vec)
+    outfile = sprintf("%s/sum.dat", outdir)
+    write(t(sum.df), file=outfile, ncolumns = 5)
+
+    h.rec.vec = A.mat %*% x
+
+    lc.rec = cbind(data.df[, 1], h.rec.vec)
+    outfile = sprintf("%s/rec.dat", outdir)
+    write(t(lc.rec), file=outfile, ncolumns = 2)
+
+    outfile = sprintf("%s/x.dat", outdir)
+    x.mat = cbind(c(1:(length(x)/2),1:(length(x)/2)), x)
+    write(t(x.mat), file=outfile, ncolumns = 2)
+
+
+    ##
+    ## lagplus
+    ##
+    h.rec.lagplus.vec = A.mat %*% x.lagplus
+
+    lc.lagplus.rec = cbind(data.df[, 1], h.rec.lagplus.vec)
+    outfile = sprintf("%s/rec_lagplus.dat", outdir)
+    write(t(lc.lagplus.rec), file=outfile, ncolumns = 2)
+
+    outfile = sprintf("%s/x_lagplus.dat", outdir)
+    x.lagplus.mat = cbind(c(1:(length(x.lagplus)/2),1:(length(x.lagplus)/2)), x.lagplus)
+    write(t(x.lagplus.mat), file=outfile, ncolumns = 2)
+
+    ##
+    ## power
+    ##
+    power.opt.lagplus = 0.0
+    power.xray.lagplus = 0.0
+    power.opt.lagminus = 0.0
+    power.xray.lagminus = 0.0    
+    for(i in 1:N){
+        power.opt.lagplus   = power.opt.lagplus + x.lagplus[i]**2 + x.lagplus[i + N]**2
+        power.xray.lagplus  = power.xray.lagplus + x.lagplus[i + 2 * N]**2 + x.lagplus[i + 3 * N]**2
+        power.opt.lagminus  = power.opt.lagminus + x.lagminus[i]**2 + x.lagminus[i + N]**2
+        power.xray.lagminus = power.xray.lagminus + x.lagminus[i + 2 * N]**2 + x.lagminus[i + 3 * N]**2        
+    }
+
+    printf("power.opt.lagplus  = %e\n", power.opt.lagplus)
+    printf("power.opt.lagminus = %e\n", power.opt.lagminus)
+    printf("power.xray.lagplus  = %e\n", power.xray.lagplus)
+    printf("power.xray.lagminus = %e\n", power.xray.lagminus)        
 }
-
-
-
+    
